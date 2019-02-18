6218c8bfdef1fe82049705fc2fd96e4bcc4a8cae
Jonathan Lavi <jonathan.lavi@gmail.com>
2019-02-18 15:49:31 +0000

    create Developer Class and pass test for getName()

---
+++ src/test/java/DeveloperTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Joe", "123456789Joe", 700000);
+    }
+
+    @Test
+    public void developerHasName(){
+        assertEquals("Joe", developer.getName());
+    }
+
+}
